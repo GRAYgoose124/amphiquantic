8df738fc8eb04721c2236388a91bc3d749ce0a47
Grayson Miller <grayson.miller124@gmail.com>
2024-05-08 18:01:15 -0600

    more cleanup

---
+++ scripts/minimal/setup.sh
@@ -0,0 +1,2 @@
+DEFAULT_STEPS_TO_RUN="*"
+DEFAULT_STEP_ORDER=("rust_build" "parameterize" "ionize" "solvate" "minimize" "equilibrate" "simulate")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     description="A molecular visualization tool for PDB files.",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Grayson Miller",
+    author_email="grayson.miller124@gmail.com",
     install_requires=["numpy", "matplotlib", "mpl_toolkits"],
 )
